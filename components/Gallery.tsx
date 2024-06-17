c72141aa632d8295e02730916aa2d5845df6e753
shalri <fsaonlinework777@gmail.com>
2024-06-17 22:35:10 +0800

    Mobile nav

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -4,14 +4,21 @@ import Image from "next/image";
 export default function Gallery() {
   return (
     <section className="w-full">
-      <div className="grid grid-cols-2 gap-0">
+      <div className="grid grid-cols-2">
         {/* <div className="flex"> */}
         {gallery.map((image, i) => (
-          <div className="relative h-[186px] w-full" key={i}>
+          <div
+            // className="relative col-span-2 row-span-1 h-[200px] overflow-hidden"
+            // className="relative min-h-[186px] min-w-[50%]"
+            // className="relative max-h-[360px] min-h-[180px] min-w-[50%]"
+            className="h-full w-full"
+            key={i}
+          >
             <Image
               src={image.mobileImg}
               alt="Gallery Image"
-              fill
+              width={400}
+              height={400}
               className="object-contain"
             />
           </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,36 +1,62 @@
+"use client";
+
 import Image from "next/image";
 import { navLinks } from "@/lib/data";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 export default function Header() {
+  const [isMobileNavActive, setIsMobileNavActive] = useState(false);
+
+  const hamburgerClick = () => {
+    setIsMobileNavActive(!isMobileNavActive);
+  };
   return (
-    <header className="mb-6 flex w-full items-center justify-between bg-[#3dbeff] px-6">
-      <div className="text-lg font-bold">
-        <a href="/" className="relative inline-block h-6 w-[130px]">
-          <Image src="./images/logo.svg" fill alt="Sunnyside logo" />
-        </a>
-      </div>
-      <nav className="flex items-center">
-        <ul className="">
-          {navLinks.map((link) => (
-            <li className="" key={link.url}>
-              <a href={link.url} className="">
-                {link.page}
-              </a>
-            </li>
-          ))}
-        </ul>
+    <>
+      {isMobileNavActive && (
+        <div className="min-w-screen fade-in fixed inset-0 z-10 min-h-screen bg-black/30 backdrop-blur" />
+      )}
+      <header className="flex w-full items-center justify-between bg-[#3dbeff] px-6 pt-8">
+        <div className="text-lg font-bold">
+          <a href="/" className="relative inline-block h-6 w-[130px]">
+            <Image src="./images/logo.svg" fill alt="Sunnyside logo" />
+          </a>
+        </div>
         <button
-          className="hamburger hamburger--minus"
+          className={cn(
+            "hamburger hamburger--minus fixed right-5 top-8 z-10 sm:hidden",
+            isMobileNavActive ? "is-active" : "",
+          )}
           type="button"
           aria-label="Menu"
           aria-controls="navigation"
-          aria-expanded="true"
+          aria-expanded={isMobileNavActive}
+          onClick={hamburgerClick}
         >
           <span className="hamburger-box">
             <span className="hamburger-inner"></span>
           </span>
         </button>
+      </header>
+      {/* <nav className="hidrelative mx-6 h-[300px] w-full"> */}
+      <nav className={cn("relative", isMobileNavActive ? "fade-in z-20" : "")}>
+        <ul
+          className={cn(
+            "hidden",
+            isMobileNavActive
+              ? "mobile-nav fixed left-6 right-6 top-6 z-20 mt-16 flex flex-col items-center gap-y-8 bg-ss-white px-6 py-10"
+              : "",
+          )}
+        >
+          {navLinks.map((link) => (
+            <li className="" key={link.url}>
+              <a href={link.url} className="text-xl text-ss-dark-grayish-blue">
+                {link.page}
+              </a>
+            </li>
+          ))}
+        </ul>
       </nav>
-    </header>
+    </>
   );
 }

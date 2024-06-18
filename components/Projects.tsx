fe018b64207066b301a10e85b365951457fedef5
shalri <fsaonlinework777@gmail.com>
2024-06-18 21:06:52 +0800

    Projects section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,27 +3,40 @@ import { cn } from "@/lib/utils";
 
 export default function Projects() {
   return (
-    <section className="flex flex-col items-center justify-center">
+    // <section className="flex flex-col items-center justify-center">
+    <section>
       {propositions.map((proposal, i) => (
-        <div className="flex flex-col items-center justify-center" key={i}>
-          <div className={cn("h-[280px] w-full", `${proposal.banner}`)} />
-          <div className="bg-ss-white px-6 py-16 text-center">
-            <h1 className="font-fraunces text-3xl font-bold text-ss-very-dark-desaturated-blue">
-              {proposal.heading}
-            </h1>
-            <p className="mb-8 mt-6 text-body text-ss-dark-grayish-blue">
-              {proposal.copy}
-            </p>
-            <div className="group flex cursor-pointer flex-col items-center">
-              <div className="z-[5] font-fraunces font-bold uppercase text-ss-very-dark-desaturated-blue">
-                Learn more
+        <div
+          className="flex flex-col items-center justify-center sm:flex-row"
+          key={i}
+        >
+          <div
+            className={cn(
+              "h-[280px] w-full sm:h-[600px] sm:w-[50%]",
+              `${proposal.banner}`,
+              i === 0 && "order-last",
+            )}
+          />
+          <div className="bg-ss-white px-6 py-16 text-center sm:w-[50%] sm:px-0 sm:text-left">
+            <div className="mx-auto px-6 sm:max-w-[500px]">
+              <h1 className="font-fraunces text-3xl font-bold text-ss-very-dark-desaturated-blue sm:text-[2.5rem] sm:leading-tight">
+                {proposal.heading}
+              </h1>
+              <p className="mb-8 mt-6 text-body text-ss-dark-grayish-blue">
+                {proposal.copy}
+              </p>
+              <div className="group flex cursor-pointer flex-col items-center sm:items-start">
+                <div className="z-[5] font-fraunces font-bold uppercase text-ss-very-dark-desaturated-blue">
+                  Learn more
+                </div>
+                <div
+                  className={cn(
+                    "-mt-2 h-[10px] w-[130px] rounded-full bg-black bg-ss-yellow/30 transition-all duration-300 group-hover:bg-ss-yellow/80",
+                    i === 1 &&
+                      "bg-ss-soft-red/30 group-hover:bg-ss-soft-red/80",
+                  )}
+                />
               </div>
-              <div
-                className={cn(
-                  "-mt-2 h-[10px] w-[130px] rounded-full bg-black bg-ss-yellow/30 transition-all duration-300 group-hover:bg-ss-yellow/80",
-                  i === 1 && "bg-ss-soft-red/30 group-hover:bg-ss-soft-red/80",
-                )}
-              />
             </div>
           </div>
         </div>
